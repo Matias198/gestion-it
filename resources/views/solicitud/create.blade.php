7bccb42a9a92d261dc87ce8bb81d53b28ff7c6de
Matias198 <matias.fernandez099@gmail.com>
2023-06-21 22:50:51 -0300

    abm y flujo funcional de solicitudes, manejo de roles y permisos

--- resources/views/solicitud/create.blade.php
+++ resources/views/solicitud/create.blade.php
@@ -45,7 +45,7 @@
                 <button type="submit"
                     class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Crear
                     solicitud</button>
-                <input type="hidden" id="comps" name="componentes" value="" style="color:black"> 
+                <input type="" id="comps" name="componentes" value="" style="color:black"> 
             </form>
         </div>
     </div>
@@ -82,5 +82,12 @@
                 }
             });
         });
+
+        componentes.forEach(function(checkbox){
+            if (checkbox.checked) {
+                // El valor ya está presente en comps.value, lo quitamos
+                comps.value += checkbox.value + ",";
+            }
+        })
     </script>
 @endsection
